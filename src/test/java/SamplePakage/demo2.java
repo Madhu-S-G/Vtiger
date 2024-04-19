ae8bda8faf9a60a8e7373151e206f38e75364eec
Madhu-S-G <madhusg.a8@gmail.com>
2024-04-19 14:47:25 -0700

    thirdcomit

---
+++ src/test/java/SamplePakage/demo2.java
@@ -0,0 +1,18 @@
+package SamplePakage;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=9;
+		int b=1;
+		int c= a+b;
+		System.out.println(c);
+		
+		
+		
+		
+	}
+
+}
